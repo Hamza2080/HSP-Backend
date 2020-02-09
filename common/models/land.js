bf19a1536c14809f9b34c4fc1966eecd3e88c4ca
HamzaJaved2080 <hamzajaved2080@gmail.com>
2020-02-10 01:22:51 +0500

    land submit installment work done

--- common/models/land.js
+++ common/models/land.js
@@ -3,32 +3,32 @@
 module.exports = function (Land) {
 
   //validity of name field...
-  // Land.validatesUniquenessOf('name');
+  Land.validatesUniquenessOf('name');
 
   // operation hook before save on land for checking information....
   Land.observe("before save", async function (ctx, next) {
     try {
-    if (ctx.instance && ctx.isNewInstance) {
-      let instance = ctx.instance;
-      if (instance.isOnInstallment) {
-        await createInstallments(instance, next);
-        next();
-      }
-      else if (instance.totalPayment < instance.downPayment + instance.discount) throw("Total payment for land must be greater then or equal to downPayment + discount.");
-      else if (instance.totalPayment > instance.downPayment + instance.discount) throw(`Total payment is greater then downPayment + discount, please submit whole payment or book plot on installment.`);
-      else if (instance.totalPayment == instance.downPayment + instance.discount && !instance.isOnInstallment) {
-        instance.landPaymentStatus = "Done";
-        instance.installments = [];
-        await putLandInfoWhileCreate(instance);
-        next();
-      } else throw("Invalid Input data, please contact support.")
-    } else throw("Error while creting new instance of Land.");
-  } catch(error) {
-    next(error)
-  }
+      if (ctx.instance && ctx.isNewInstance) {
+        let instance = ctx.instance;
+        if (instance.isOnInstallment) {
+          await createInstallments(instance, next);
+          next();
+        } else if (instance.totalPayment < instance.downPayment + instance.discount) throw ("Total payment for land must be greater then or equal to downPayment + discount.");
+        else if (instance.totalPayment > instance.downPayment + instance.discount) throw (`Total payment is greater then downPayment + discount, please submit whole payment or book plot on installment.`);
+        else if (instance.totalPayment == instance.downPayment + instance.discount && !instance.isOnInstallment) {
+          instance.landPaymentStatus = "Done";
+          instance.installments = [];
+          await putLandInfoWhileCreate(instance);
+          next();
+        } else throw ("Invalid Input data, please contact support.")
+      } else if (ctx.data) next();
+      else throw ("Error while creting new instance of Land.");
+    } catch (error) {
+      next(error)
+    }
   });
 
-  function putLandInfoWhileCreate(instance) {    
+  function putLandInfoWhileCreate(instance) {
     return new Promise(async (resolve, reject) => {
       try {
         Promise.all([
@@ -46,10 +46,9 @@ module.exports = function (Land) {
           if (landMeasuringData) instance["measuring_unitData"] = landMeasuringData;
           if (landlordData) instance["landlordData"] = landlordData;
           if (plotPaymentPlanData) instance["plotPaymentPlanData"] = plotPaymentPlanData;
-          console.log(instance)
           resolve();
         })
-      } catch(error) {
+      } catch (error) {
         reject(error);
       }
     })
@@ -108,8 +107,8 @@ module.exports = function (Land) {
     return new Promise(async (resolve, reject) => {
       try {
         let paymentPlan = await Land.app.models.plot_payment_plan.findById(instance.plotPaymentPlanId);
-        if (!paymentPlan) throw(`Please provide a valid PaymentPlan Id, ${instance.plotPaymentPlanId} is not a valid paymentPlan Id.`);
-        else if (paymentPlan){
+        if (!paymentPlan) throw (`Please provide a valid PaymentPlan Id, ${instance.plotPaymentPlanId} is not a valid paymentPlan Id.`);
+        else if (paymentPlan) {
           let totalInstallments = paymentPlan.no_of_installment;
           let remainingAmount = instance.totalPayment - instance.downPayment - instance.discount;
           let installmentAmount = paymentPlan.installmentAmount;
@@ -117,7 +116,7 @@ module.exports = function (Land) {
           let purchaseDate = instance.acquired_date;
           let installments = [];
 
-          for (let i = 0; i< totalInstallments; i++) {
+          for (let i = 0; i < totalInstallments; i++) {
             let date = new Date(purchaseDate);
             let amount = remainingAmount >= installmentAmount ? installmentAmount : remainingAmount;
             installments.push({
@@ -135,7 +134,6 @@ module.exports = function (Land) {
           resolve();
         }
       } catch (error) {
-        console.log(error)
         reject(error);
       }
     })
@@ -161,11 +159,6 @@ module.exports = function (Land) {
         required: true
       },
       {
-        arg: 'amount',
-        type: 'number',
-        required: true
-      },
-      {
         arg: 'landId',
         type: 'string',
         required: true
@@ -177,46 +170,54 @@ module.exports = function (Land) {
       root: true
     }
   });
-  Land.submitInstallment = async function (submittedBy, submittedTo, contact, amount, landId, cb) {
+  Land.submitInstallment = async function (submittedBy, submittedTo, contact, landId, cb) {
     try {
       let landInfo = await Land.findById(landId);
-      if (landInfo.installments.length > 0 && landInfo.landPaymentStatus != "Done") {
-        let length = landInfo.installments.length - 1;
-        if (landInfo.installments[length].status == "pending") {
-          let installments = landInfo.installments[length].installments;
-          let iteration = 0;
-          let pendingInstallment = true;
-          while (iteration <= installments.length && pendingInstallment) {
-            if (installments[iteration].status == "pending") {
-              pendingInstallment = false;
-              if (amount == installments[iteration].installmentAmount) {
-                installments[iteration].status = "done";
-                installments[iteration].submittedOn = new Date();
-                installments[iteration].submittedBy = submittedBy;
-                installments[iteration].submittedTo = submittedTo;
-                installments[iteration].contact = contact;
-                installments[iteration].amount = amount;
+      if (landInfo && landInfo.installments.length > 0 && landInfo.landPaymentStatus != "Done") {
+        let landPaymentPlan = landInfo.paymentPlanData;
+        let installmentAmount = landPaymentPlan.installmentAmount;
+        let allInstallments = landInfo.installments;
 
-                if (landInfo.installments[length].installment[landInfo.installments[length].installment.length - 1].status == "done") {
-                  landInfo.installments[length].status = "done";
-                  landInfo.landPaymentStatus == "Done";
-                } else landInfo.landPaymentStatus == "notDone";
-                landInfo.installments[length].installments = installments;
-
-                await saveInstallmentsInDB(landId, landInfo);
-                cb(null, {
-                  ...landInfo
-                })
-              } else cb(`Installment amount must be equal to ${installments[iteration].installmentAmount}.`)
-            }
-            iteration++;
+        let isSubmitted = false;
+        for (let i=0; i< landPaymentPlan.no_of_installment; i++) {
+          if (allInstallments[i].status == "pending" && !isSubmitted) {
+            isSubmitted = true;
+            landInfo.installments[i].status = "done";
+            landInfo.installments[i].submittedOn = new Date();
+            landInfo.installments[i].submittedBy = submittedBy;
+            landInfo.installments[i].submittedTo = submittedTo;
+            landInfo.installments[i].contact = contact;
           }
-        } else cb(`Land with landId ${landId} installment status is not done, please contact support if you need any help.`)
+        }
+        if (allInstallments[allInstallments.length - 1].status == "done") {
+          landInfo.landPaymentStatus = "Done";
+        }
+        await Land.upsert(landInfo);
+        cb(null, {
+          ...landInfo
+        })
       } else {
-        cb(`Land with landId ${landId} payment status is already done, please verify.`)
+        console.log("inside")
+        throw(`Land with landId ${landId} payment status is already done, please verify.`)
       }
     } catch (err) {
+      console.log("here inside")
+
+      console.log(err)
       cb(err);
     }
+  }
+
+  // save land info after creating installment array...
+  function saveInstallmentsInDB(landId, landInfo) {
+    return new Promise(async (resolve, reject) => {
+      
+      try {
+console.log()
+        resolve(await Land.upsert(landInfo));
+      } catch (err) {
+        reject(err);
+      }
+    })
   }
 };
