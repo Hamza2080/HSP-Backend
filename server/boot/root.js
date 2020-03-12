f0c5208009f3c5940b2a019a382cb257ed83677f
Hamza2080 <36309525+Hamza2080@users.noreply.github.com>
2020-03-12 18:43:51 +0500

    add paypal_test route for testing

--- server/boot/root.js
+++ server/boot/root.js
@@ -4,5 +4,10 @@ module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
+  router.get('/paypal_test', (req, res) => {
+    console.log("paypal_test route hit successfully.");
+    console.log(req.query);
+    res.send("Hello");
+  });
   server.use(router);
 };
