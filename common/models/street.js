0dbc094821f0cbbea6d01edb3811cd03086465b6
HamzaPLT <Hamza.Javed@prettylittlething.com>
2020-06-14 23:02:22 +0500

    street.js

--- common/models/street.js
+++ common/models/street.js
@@ -1,5 +1,34 @@
 'use strict';
 
-module.exports = function(Street) {
+module.exports = function (Street) {
+    /**
+   * get street against town...
+   */
+    Street.remoteMethod("getStreetByTownId", {
+        accepts: [{
+            arg: "townId",
+            type: "string",
+            required: true
+        }
+        ],
+        http: {
+            path: "/getStreetByTownId",
+            verb: "get"
+        },
+        description: "Get streets against townId.",
+        returns: {
+            arg: "data",
+            type: "object",
+            root: true
+        }
+    });
 
+    Street.getStreetByTownId = async function (townId, cb) {
+        try {
+            let streets = await Street.find({"townId": townId});
+            cb (null, streets || []);
+        } catch (err) {
+            cb(err);
+        }
+    };
 };
